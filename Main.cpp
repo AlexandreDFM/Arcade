44c541adc52d490c57bbb49fb70d54e4fb0b66f1
RoroLopes <roman.lopes@epitech.eu>
2023-03-28 13:50:02 +0200

    Add Core open lib

--- Main.cpp
+++ Main.cpp
@@ -9,10 +9,10 @@
 #include "Menu/Menu.hpp"
 #include "Usage/Usage.hpp"
 #include "Libraries/SFML/SFML.hpp"
+#include "Core/Core.hpp"
 #include "Libraries/NCurses/NCurses.hpp"
 #include "Libraries/SDL2/SDL2.hpp"
 #include "DLLoader/DLLoader.hpp"
-#
 
 int main(int argc, char **argv)
 {
@@ -20,48 +20,5 @@ int main(int argc, char **argv)
     if (argc == 2 && std::string(argv[1]) == "-h") {
         Usage::DisplayUsage(); return 0;
     }
-    DLLoader<Arcade::IDisplay> loader("./lib/lib_arcade_ncurses.so");
-    auto instance = loader.getInstance("create");
-    instance->init();
-//    while(instance->isRunning()) {
-//        instance->update();
-//    }
-//    instance->stop();
-//    instance->~IDisplayModule();
-//    return 0;
-
-//    menu->init();
-//    std::vector <std::string> gameList = menu->getGame();
-//    std::vector <std::string> libList = menu->getLib();
-//    std::cout << "Games List:" << std::endl;
-//    for (auto &i : gameList) std::cout << i << std::endl;
-//    std::cout << std::endl <<"Library List:" << std::endl;
-//    for (auto &i : libList) std::cout << i << std::endl;
-//
-//    game->init();
-//    while(game->getIsRunning()) {
-//        game->drawText("Hello", 0, 0);
-//        game->update();
-//    }
-    // Menu menu;
-    // menu.start();
-
-    // Usage usage;
-    // usage.CheckUsage(argc, argv);
-    // Menu menu;
-    // ncurse ncurse;
-    // menu.init();
-    // std::vector <std::string> gamelist = menu.getGame();
-    // std::vector <std::string> liblist = menu.getLib();
-    // ncurse.init();
-    // while (1)
-    // {
-    //     ncurse.drawText(liblist[0], 0, 0);
-    //     ncurse.drawText(liblist[1], 0, 1);
-    //     ncurse.drawText(liblist[2], 0, 2);
-    //     ncurse.drawText(gamelist[0], 0, 5);
-    //     ncurse.update();
-    // }
-
-    return 0;
+    Arcade::Core("./lib/lib_arcade_ncurses.so");
 }
