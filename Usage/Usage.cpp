804b66278193e3134a5b1ed33e09560a392c759d
RoroLopes <roman.lopes@epitech.eu>
2023-04-08 14:11:34 +0200

    Add: Add usages

--- Usage/Usage.cpp
+++ Usage/Usage.cpp
@@ -26,7 +26,17 @@ void Usage::DisplayUsage()
     std::cout << "USAGE" << std::endl;
     std::cout << "\t./arcade Libraries" << std::endl;
     std::cout << "DESCRIPTION" << std::endl;
-    std::cout << "\tLibraries\t\tlibrary in which the game will be played" << std::endl;
+    std::cout << "\tLibraries\t\tlibrary in which the game will be played" << std::endl << std::endl;
+    std ::cout << "\tEscape Key\t\tClose the program" << std::endl;
+    std::cout << "\tArrow Keys\t\tFor movements in game" << std::endl;
+    std::cout << "\tSpace Key\t\tFor reset games" << std::endl;
+    std::cout << "\tM Key\t\t\tFor graphical next library" << std::endl;
+    std::cout << "\tL Key\t\t\tFor graphical previous library" << std::endl;
+    std::cout << "\tO Key\t\t\tFor game previous library" << std::endl;
+    std::cout << "\tP Key\t\t\tFor game next library" << std::endl;
+    std::cout << "\tR Key\t\t\tFor validate choices in menu and action1" << std::endl;
+    std::cout << "\tE Key\t\t\tFor action2" << std::endl;
+    std::cout << "\tY Key\t\t\tFor going back to menu" << std::endl;
 }
 
 void Usage::CheckUsage(int ac, char **av)
