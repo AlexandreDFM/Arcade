19d81a88d53aa9559c4aa230ffad8e490a6554b1
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-04-09 22:12:31 +0200

    feature: adding documentation for Usage.

--- Usage/Usage.cpp
+++ Usage/Usage.cpp
@@ -7,6 +7,14 @@
 
 #include "Usage.hpp"
 
+/**
+ * The function Error() sets an error message based on the type of error passed as
+ * an argument.
+ *
+ * @param type The parameter "type" is an enum type variable of ErrorType, which
+ * is used to determine the type of error that occurred. It can have two possible
+ * values: LIB or GAME.
+ */
 Usage::Error::Error(ErrorType type)
 {
     switch (type) {
@@ -19,8 +27,20 @@ Usage::Error::Error(ErrorType type)
     }
 }
 
-const std::string &Usage::Error::what() const { return _message; }
+/**
+ * This function returns the error message associated with an exception.
+ *
+ * @return A constant reference to a string object, which is the error message
+ * stored in the private member variable `_message`.
+ */
+const std::string &Usage::Error::what() const
+{
+    return _message;
+}
 
+/**
+ * The function displays the usage and controls for a program.
+ */
 void Usage::DisplayUsage()
 {
     std::cout << "USAGE" << std::endl;
@@ -36,9 +56,21 @@ void Usage::DisplayUsage()
     std::cout << "\tP Key\t\t\tFor game next library" << std::endl;
     std::cout << "\tR Key\t\t\tFor validate choices in menu and action1" << std::endl;
     std::cout << "\tE Key\t\t\tFor action2" << std::endl;
+    std::cout << "\\T Key\t\t\tFor saving the game" << std::endl;
     std::cout << "\tY Key\t\t\tFor going back to menu" << std::endl;
 }
 
+/**
+ * The function checks the command line arguments and displays usage information
+ * or exits the program with an error code.
+ *
+ * @param ac `ac` is an integer representing the number of arguments passed to the
+ * program from the command line.
+ * @param av `av` is a pointer to a character array, which represents the command
+ * line arguments passed to the program. Each element of the array is a string
+ * containing one of the arguments. In this case, `av` is being used to check if
+ * the second argument is "-h" to display the usage
+ */
 void Usage::CheckUsage(int ac, char **av)
 {
     if (ac != 2) {
@@ -52,6 +84,15 @@ void Usage::CheckUsage(int ac, char **av)
     }
 }
 
+/**
+ * The function checks if the input library file has a ".so" extension and throws
+ * an error if it doesn't.
+ *
+ * @param av `av` is a pointer to a character array (i.e., a string array) that
+ * contains the command line arguments passed to the program. The first element of
+ * this array (`av[0]`) is typically the name of the program itself, and
+ * subsequent elements (`av[1]`, `
+ */
 void Usage::CheckLib(char **av)
 {
     std::string lib = av[1];
