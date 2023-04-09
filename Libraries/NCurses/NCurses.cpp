9b6b6cee5ce0a893ddbc85ff6e7acc9487d85c72
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-04-09 21:31:24 +0200

    feature: adding documentation for NCurses

--- Libraries/NCurses/NCurses.cpp
+++ Libraries/NCurses/NCurses.cpp
@@ -8,11 +8,25 @@
 #include "NCurses.hpp"
 
 namespace Arcade {
+    /**
+     * This is a constructor for the NCurses class that initializes the window
+     * pointer to nullptr.
+     */
     NCurses::NCurses()
     {
         this->window = nullptr;
     }
 
+    /**
+     * The function initializes the NCurses library and sets up color pairs for
+     * the game.
+     *
+     * @param gameAssets gameAssets is a map that contains character keys and
+     * their corresponding string values. It is passed as a parameter to the
+     * init() function of the NCurses class. However, in the given code, the
+     * gameAssets parameter is not used and is cast to void to avoid a compiler
+     * warning.
+     */
     void NCurses::init(const std::map<char, std::string> &gameAssets)
     {
         (void) gameAssets;
@@ -30,6 +44,14 @@ namespace Arcade {
         init_pair(8, COLOR_BLACK, COLOR_YELLOW);
     }
 
+    /**
+     * This function updates the NCurses window every 0.1 seconds.
+     *
+     * @return If the elapsed time since the last update is less than 0.1 seconds,
+     * then nothing is returned and the function exits early. If the elapsed time
+     * is greater than or equal to 0.1 seconds, then the function refreshes the
+     * window and returns nothing.
+     */
     void NCurses::update()
     {
         static auto start = std::chrono::steady_clock::now();
@@ -40,6 +62,14 @@ namespace Arcade {
         wrefresh(this->window);
     }
 
+    /**
+     * The function clears the NCurses window if at least 0.1 seconds have passed
+     * since the last clear.
+     *
+     * @return If the elapsed time since the last call to `clear()` is less than
+     * 0.1 seconds, then nothing is done and the function returns. Otherwise, the
+     * `window` is cleared using the `wclear()` function.
+     */
     void NCurses::clear()
     {
         static auto start = std::chrono::steady_clock::now();
@@ -50,11 +80,23 @@ namespace Arcade {
         wclear(this->window);
     }
 
+    /**
+     * The function closes the NCurses window.
+     */
     void NCurses::close()
     {
         endwin();
     }
 
+    /**
+     * The function displays a vector of Drawable objects with different colors
+     * using the NCurses library.
+     *
+     * @param drawables A vector of Drawable objects that contain information
+     * about the position and color of objects to be displayed on the screen. The
+     * function iterates through this vector and displays each object on the
+     * screen with the specified color.
+     */
     void NCurses::display(std::vector<Drawable> drawables)
     {
         for (auto &drawable : drawables) {
@@ -84,6 +126,16 @@ namespace Arcade {
         }
     }
 
+    /**
+     * This function displays a vector of DrawableText objects with different
+     * colors using the NCurses library.
+     *
+     * @param drawables The parameter `drawables` is a vector of `DrawableText`
+     * objects. Each `DrawableText` object contains information about a text
+     * string to be displayed on the screen, including its position (x and y
+     * coordinates) and color. The `display` function uses this information to
+     * print the text strings
+     */
     void NCurses::display(std::vector<DrawableText> drawables)
     {
         for (auto &drawable : drawables) {
@@ -113,6 +165,16 @@ namespace Arcade {
         }
     }
 
+    /**
+     * The function returns an Arcade event type based on the key pressed in the
+     * NCurses window.
+     *
+     * @return The function `getEvent()` returns an `Arcade::EventType` enum value
+     * based on the user input received through the `ncurses` library. The
+     * returned value corresponds to the type of event that occurred, such as a
+     * key press or a menu selection. If no event occurred, the function returns
+     * `Arcade::EventType::NOTHING`.
+     */
     Arcade::EventType NCurses::getEvent()
     {
         switch (wgetch(this->window)) {
@@ -134,11 +196,30 @@ namespace Arcade {
         }
     }
 
+    /* The `extern "C"` block is used to specify that the functions inside it
+    should be compiled using C linkage instead of C++ linkage. This is
+    necessary when creating a shared library that will be used by programs
+    written in other languages, such as C. */
     extern "C" {
+        /**
+         * The function returns a new instance of the NCurses class that
+         * implements the IDisplay interface.
+         *
+         * @return A pointer to a new instance of the `NCurses` class, which
+         * implements the `IDisplay` interface.
+         */
         IDisplay *entryPoint()
         {
             return new NCurses();
         }
+
+        /**
+         * The function returns a string indicating the type of library as
+         * "libNcurses".
+         *
+         * @return A string literal "libNcurses" is being returned as a pointer to
+         * a character array.
+         */
         char *getType()
         {
             return (char *) "libNcurses";
